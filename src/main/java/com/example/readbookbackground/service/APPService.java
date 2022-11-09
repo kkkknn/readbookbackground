27415eb58f782a696ff9dfea2ca738940ed9ab35
kkkkkn <1185553557@qq.com>
2022-11-09 11:17:49 +0800

    修改APK信息文件目录

--- src/main/java/com/example/readbookbackground/service/APPService.java
+++ src/main/java/com/example/readbookbackground/service/APPService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class APPService {
     //存放APP目录
-    private final String path="/home/clonebook_application/apk_files/";
+    private final String path="/home/ubuntu/clone_project/apk_files/";
 
     public String getVersionInfo(){
         //读取指定文件目录json
